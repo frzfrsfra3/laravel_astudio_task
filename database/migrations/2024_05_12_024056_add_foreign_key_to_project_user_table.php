6c80b97596b332f466d29a8ddc8a37991fd982c7
feras <frzfrsfra3@gmail.com>
2024-05-12 07:44:28 +0500

    add foreign keys

--- database/migrations/2024_05_12_024056_add_foreign_key_to_project_user_table.php
+++ database/migrations/2024_05_12_024056_add_foreign_key_to_project_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddForeignKeyToTimesheetsTable extends Migration
+class AddForeignKeyToProjectUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class AddForeignKeyToTimesheetsTable extends Migration
      */
     public function up()
     {
-       
-        Schema::table('timesheets', function (Blueprint $table) {
-            
-
-            // Add foreign key constraints
+        Schema::table('project_user', function (Blueprint $table) {
+            //
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+     
         });
     }
 
@@ -30,10 +28,11 @@ class AddForeignKeyToTimesheetsTable extends Migration
      */
     public function down()
     {
-        Schema::table('timesheets', function (Blueprint $table) {
+        Schema::table('project_user', function (Blueprint $table) {
             //
-            $table->dropForeign(['user_id']);
-            $table->dropForeign(['project_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+     
         });
     }
 }
