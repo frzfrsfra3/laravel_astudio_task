d15ec5cfee3fee57a4276b1ba8506585e765a1f2
feras <frzfrsfra3@gmail.com>
2024-05-12 07:39:22 +0500

    add foreign key

---
+++ database/migrations/2024_05_12_022915_add_foreign_key_to_timesheets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       
+        Schema::table('timesheets', function (Blueprint $table) {
+            
+
+            // Add foreign key constraints
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('timesheets', function (Blueprint $table) {
+            //
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['project_id']);
+        });
+    }
+}
